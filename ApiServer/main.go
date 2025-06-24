e9812b0cae74946d1338bba0b7a8f42cf31224d1
Tal Debby <taldebby2004@gmail.com>
2025-06-24 15:38:28 +0300

    refactor(main.go): seperate concernce at main

--- ApiServer/main.go
+++ ApiServer/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"os/signal"
 	languagesCrud "server/crud"
 	"server/middleware"
 	"server/routes"
 	languagesService "server/service"
+	"sync"
+	"time"
 )
 
-func main() {
-	fmt.Println("Starting Server")
-
+func initHttpServer() *http.Server {
 	languagesRepository := languagesCrud.NewJsonWordsRepository()
 	languagesService := languagesService.NewLanguagesService(languagesRepository)
 
@@ -25,7 +29,55 @@ func main() {
 		middleware.CORS,
 	)
 
-	if err := http.ListenAndServe("localhost:8080", defaultMiddlewars(mux)); err != nil {
+	httpServer := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: defaultMiddlewars(mux),
+	}
+
+	return httpServer
+}
+
+func startHttpServer(httpServer *http.Server) {
+	fmt.Println("Starting Server")
+	if err := httpServer.ListenAndServe(); err != nil {
 		fmt.Println(err.Error())
+	}
+}
+
+func shutdownHttpServer(httpServer *http.Server, ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Wait for the context to be done (e.g., on interrupt signal)
+	<-ctx.Done()
+	fmt.Println("Shutting down server...")
+	shutdownCtx := context.Background()
+	shutdownCtx, cancel := context.WithTimeout(shutdownCtx, 10*time.Second)
+	defer cancel()
+	if err := httpServer.Shutdown(shutdownCtx); err != nil {
+		fmt.Fprintf(os.Stderr, "error shutting down http server: %s\n", err)
+	}
+}
+
+func run(ctx context.Context, w io.Writer, args []string) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	httpServer := initHttpServer()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go startHttpServer(httpServer)
+	go shutdownHttpServer(httpServer, ctx, &wg)
+
+	wg.Wait()
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	if err := run(ctx, os.Stdout, os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running server: %v\n", err)
+		os.Exit(1)
 	}
 }
