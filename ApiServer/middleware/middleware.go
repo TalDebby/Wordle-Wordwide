d22802659582b55aa909be466b63fc32c5cece65
TalDebby <75690831+TalDebby@users.noreply.github.com>
2025-07-19 10:07:59 +0300

    Basic middleware (#4)
    * feat(several): cors + recovery middlewares
    
    * feat(middleware.go): add admin authorization middleware
    
    * refactor(main.go): seperate concernce at main
    
    * feat(middleware.go): add dependency injection to logger
    
    * test(middleware_test.go): add tests
    
    * feat(middleware.go, middleware_test.go): add only admin tests

--- ApiServer/middleware/middleware.go
+++ ApiServer/middleware/middleware.go
@@ -1,22 +1,13 @@
 package middleware
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"time"
 )
 
 type Middleware func(next http.Handler) http.Handler
-
-type wrappedResponseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (w *wrappedResponseWriter) WriteHeader(statusCode int) {
-	w.statusCode = statusCode
-	w.ResponseWriter.WriteHeader(statusCode)
-}
 
 func CreateStack(middlewares ...Middleware) Middleware {
 	stackedMiddleware := func(next http.Handler) http.Handler {
@@ -29,17 +20,72 @@ func CreateStack(middlewares ...Middleware) Middleware {
 	return stackedMiddleware
 }
 
-func Logging(next http.Handler) http.Handler {
+type wrappedResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *wrappedResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func Logging(w io.Writer) func(next http.Handler) http.Handler {
+	logger := log.New(w, "", log.Default().Flags())
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			startTime := time.Now()
+			logger.Printf("Start request: %s %s", r.Method, r.URL.Path)
+			wrappedWriter := &wrappedResponseWriter{
+				ResponseWriter: w,
+				statusCode:     http.StatusOK,
+			}
+
+			next.ServeHTTP(wrappedWriter, r)
+
+			logger.Printf("Finish request: %d %s %s %s", wrappedWriter.statusCode, r.Method, r.URL.Path, time.Since(startTime))
+		})
+	}
+}
+
+func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		startTime := time.Now()
-		log.Printf("Start request: %s %s", r.Method, r.URL.Path)
-		wrappedWriter := &wrappedResponseWriter{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		// Handle allowed CORS (empty request)
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
 		}
 
-		next.ServeHTTP(wrappedWriter, r)
+		next.ServeHTTP(w, r)
+	})
+}
 
-		log.Printf("Finish request: %d %s %s %s", wrappedWriter.statusCode, r.Method, r.URL.Path, time.Since(startTime))
+// Recovery middleware to handle panics and return a 500 Internal Server Error
+func Recovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("Recovered from panic: %v", err)
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func OnlyAdmin(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		isAdmin := r.Header.Get("Authorization") == "admin"
+
+		if !isAdmin {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
 	})
 }
